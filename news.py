696829a8adfcef1a54b4ba2e482f8a70a4d4c78c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-11-10 20:44:11 +0000

    Add files via upload

--- news.py
+++ news.py
@@ -1,16 +1,21 @@
 import re
+import re
 from bs4 import BeautifulSoup
 import requests
 
 
+print("\x1bc\x1b[43;30m")
 
 page = requests.get("https://github.com/codebuil?tab=repositories")
 s=BeautifulSoup(page.content,'html.parser')
-ss=s.select_one("h3.wb-break-all")
-sss=ss.select_one("a")
+ss=s.select_one("body")
 
-print("\x1bc\x1b[43;30m")
+ss=ss.select_one("main")
+ss=ss.select("h3")
+for n in list(ss):
+    sss=n.select_one("a")
+    print(sss.string.strip())
+    #print(n.strip())
 
-print("codebuil site news code:")
-print(sss.string)
+
 
